95345466c1b50febd63370cba69be3cfc855e91e
Miroslav Vitkov <sir.vorac@gmail.com>
2017-11-01 13:16:28 +0200

    Second subassignemns: generate text.

--- assignment1/ans.py
+++ assignment1/ans.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Assignment 1 for Natural Language Processing.
-Does word frequency plots, 
+"""
+Assignment 1 for Natural Language Processing.
+Does word frequency plots, Dissociated Press generation
+and Pointwise mutual information.
 """
 
 
 import matplotlib.pyplot as plt
+from ngram import NgramModel
 import nltk
 import numpy as np
 
@@ -13,58 +16,60 @@ import numpy as np
 # nltk.download('punkt')
 
 
-KJBIBLE = "res/kjbible.txt"
-JBOOK = "res/junglebook.txt"
-SETIMES_TR = "res/SETIMES2.bg-tr.tr"
-SETIMES_BG = "res/SETIMES2.bg-tr.bg"
+KJBIBLE = "kjbible.txt"
+JBOOK = "junglebook.txt"
+SETIMES_TR = "SETIMES2.bg-tr.tr"
+SETIMES_BG = "SETIMES2.bg-tr.bg"
 FILES = (KJBIBLE, JBOOK, SETIMES_TR, SETIMES_BG)
 
 
-def calc_freq(fname):
-    """fname - path to a plaintext UTF8 file
-    returns - np.array of sorted word frequencies
-    """
+def tokenize(fname):
+    """Return a list of all the tokens in a text file."""
     with open(fname, 'r') as f:
         text = f.read().lower()
         tokens = nltk.tokenize.word_tokenize(text)
-        dist = nltk.FreqDist(tokens)
-        # dist.plot(100)  # nice and informative
-        return dist_to_sorted_list(dist)
+        return tokens
 
 
-def dist_to_sorted_list(dist):
-    """d - a FreqDist object
-    returns - descending sorted np.array
-    """
+def calc_freq(tokens):
+    """Return descending word frequencies."""
+    dist = nltk.FreqDist(tokens)
+    # dist.plot(100)  # nice and informative
     freqs = np.array(list(dist.values()))  # only one copy
     freqs[::-1].sort()  # in-place
     return freqs
 
 
-def plot_freq(freqs, n=0):
-    """freqs - list of integer magnitudes
-    n - plot counter, 0-based
-    returns - None
-    side effects - plots linear and log graphs
-    """
-    corpora = len(FILES)
-    width = 2  # linlin and loglog
-    plt.subplot(corpora, width, n*2+1)
-    plt.plot(freqs)
-    plt.subplot(corpora, width, n*2+2)
-    plt.loglog(freqs)
+def plot_freqs(files=FILES):
+    """Plot word frequency statistics in requested text files."""
+    freqs = [calc_freq(x) for x in files]
 
+    def plot_one(f, n):
+        corpora = len(files)
+        width = 2  # linlin and loglog
+        plt.subplot(corpora, width, n*2+1)
+        plt.plot(f)
+        plt.subplot(corpora, width, n*2+2)
+        plt.loglog(f)
 
-def plot_all_freqs():
-    """
-    """
-    freqs = [calc_freq(x) for x in FILES]
     for i, val in enumerate(freqs):
-        plot_freq(val, i)
+        plot_one(val, i)
     plt.show()
+
+
+def gen_disassociated_press(order=8, len=100):
+    """Generate some autocorrelated text."""
+    tokens = [k for k in tokenize(KJBIBLE) if k.isalpha()]
+    model = NgramModel(order, tokens, nltk.probability.MLEProbDist)
+    ret = ["",] * (order-1)
+    for i in range(len):
+        c = ret[-(order-1):]
+        print(c)
+        ret.append(model.generate_one(c))
+    return ' '.join(ret)
 
 
 if __name__ == "__main__":
     print('my name: Miroslav Vitkov')
-    plot_all_freqs()
-    print('first assignment presented')
+    plot_freqs()
+    print(gen_disassociated_press())  # the bible is by far the most fun to use here
