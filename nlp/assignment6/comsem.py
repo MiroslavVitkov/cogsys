f517bd9e4a0ef2b8f6489d866be668450cc6ef65
Miroslav Vitkov <sir.vorac@gmail.com>
2018-01-31 22:57:48 +0100

    [nlp] First problem completed. That was too easy.

--- nlp/assignment6/comsem.py
+++ nlp/assignment6/comsem.py
@@ -22,11 +22,17 @@ def parse(g, s):
 
 
 def main():
-    grammar = load('grammars/sample_grammars/sem2.fcfg')
-    grammar = extend_grammar(grammar, r"N[NUM='sg', SEM=<\x.god(x)>] -> 'god'")
-    sem = parse(grammar, 'Mary chases a god')
+    g = load('grammars/sample_grammars/sem2.fcfg')
+
+    g = extend_grammar(g, r"N[NUM='sg', SEM=<\x.story(x)>] -> 'story'")
+
+    g = extend_grammar(g, r"TV[NUM='sg', SEM=<\X y.X(\x.tell(y,x))>, TNS='pres'] -> 'tells'")
+    g = extend_grammar(g, r"TV[NUM='pl', SEM=<\X y.X(\x.tell(y,x))>, TNS='pres'] -> 'tell'")
+
+    g = extend_grammar(g, r"VP[NUM=?n, SEM=<?v(?obj1, ?obj2)>] -> TV[NUM=?n, SEM=?v] NP[SEM=?obj1] NP[SEM=?obj2]")
+
+    sem = parse(g, 'Mary tells Suzie a story')
     print(sem)
-    #print(extend_grammar(grammar, 'V -> N'))
 
 
 if __name__ == '__main__':
