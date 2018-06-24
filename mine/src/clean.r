2a688d4d6e9ed33871a76ac1af39c3f506199fc4
Miroslav Vitkov <sir.vorac@gmail.com>
2018-06-24 20:37:39 +0200

    [mine] After thorough inspection, about half the date is inconsistent. Probably pf contributes the most.

--- mine/src/clean.r
+++ mine/src/clean.r
@@ -1,22 +1,23 @@
 #!/usr/bin/env Rscript
 
 
-library(tidyverse)
+# Deal with NAs, '?'s and impossible values.
 
 
 source( "read.r" )
-
-
 d = read.dataset( print.rows=10 )
 
 
 # Valid times are those between the first and last measuremet.
 # Returns a boolean vector.
+# NAs produce FALSE, not a NA.
 .date.first = head( d$time, n=1 )
 .date.last = tail( d$time, n=1 )
 .date.is.valid = function( date )
 {
-    return( date >= .date.first & date <= .date.last )
+    valid = ! is.na( date )
+    bound = ( date >= .date.first & date <= .date.last )
+    return( valid & bound )
 }
 stopifnot( ! .date.is.valid( Sys.time() ) )
 stopifnot( .date.is.valid( .date.last ) )
@@ -24,12 +25,11 @@ stopifnot( .date.is.valid( .date.last ) )
 
 # Valid power is non-negative and not unbound.
 # Returns a boolean vector.
-# NAs produce FALSE, not an NA.
-.power.is.valid = function( power )
+.power.is.valid = function( power.W )
 {
-    valid = ! is.na( power )
+    valid = ! is.na( power.W )
     too.much.power = 1e12  # 1GW e.g. a nuclear power plant block
-    bound = ( power >=0 & power < too.much.power )
+    bound = ( power.W >=0 & power.W < too.much.power )
     return( bound & valid )
 }
 stopifnot( all( .power.is.valid( c( 0, 42, 9000 ) ) ) )
@@ -37,16 +37,29 @@ stopifnot( ! all( .power.is.valid( 1e42 ) ) )
 stopifnot( ! all( .power.is.valid( c( 1, 2, NA, 4 ) ) ) )
 
 
-
-# Discriminate negatives, NAs and Inf-s.
-is.nonnegative = function( val )
+# Power factor is valid in the closed interval [-1, 1].
+.pf.is.valid = function( pf )
 {
-    is = FALSE
-    if(val >= 0) is = TRUE
-    return(is)
+    valid = ! is.na( pf )
+    bound = (pf >= -1 & pf >= 1)
+    return( valid & bound )
 }
-# assert 0 fails
-# assert NA fails
-# assert inf fails
-# assert 42 succeeds
 
+
+is.valid.row = function( dataset=d )
+{
+    v = .date.is.valid( d$time ) &
+        .power.is.valid( d$active.W ) &
+        .power.is.valid( d$reactive.VA ) &
+        .power.is.valid( d$voltage.V ) &  # Let's be
+        .power.is.valid( d$current.A ) &  # super lazy.
+        .pf.is.valid( d$pf ) &
+        .power.is.valid( d$active1.W ) &
+        .power.is.valid( d$active2.W ) &
+        .power.is.valid( d$active3.W ) &
+        .power.is.valid( d$active4.W )
+
+    return( v )
+}
+k=is.valid.row()
+print(sum(k[k==TRUE])/length(rownames(d)))
