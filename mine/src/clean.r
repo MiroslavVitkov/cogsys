6006642ae3b0334933cd9cf777f18eaa3389d42d
Miroslav Vitkov <sir.vorac@gmail.com>
2018-06-24 14:55:04 +0200

    [mine] In R & and | are element-wise operators, && and || fold a dimension, much as in C.

--- mine/src/clean.r
+++ mine/src/clean.r
@@ -11,14 +11,18 @@ d = read.dataset( print.rows=10 )
 
 
 # Valid times are those between the first and last measuremet.
-date.first = head( d$time, n=1 )
-date.last = tail( d$time, n=1 )
-date.is.valid = function( date )
+.date.first = head( d$time, n=1 )
+.date.last = tail( d$time, n=1 )
+.date.is.valid = function( date )
 {
-    date >= date.first && date <= date.last
+    date >= .date.first & date <= .date.last
 }
-stopifnot( ! date.is.valid( Sys.time() ) )
-stopifnot( date.is.valid( date.last ) )
+stopifnot( ! .date.is.valid( Sys.time() ) )
+stopifnot( .date.is.valid( .date.last ) )
+
+
+v = date.is.valid( d$time )
+print(v)
 
 
 # Discriminate negatives, NAs and Inf-s.
