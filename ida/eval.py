0a09ff7d6569ddf9522444a96447d9705c64a55c
Miroslav Vitkov <sir.vorac@gmail.com>
2018-07-13 11:02:05 +0200

    [ida] BUG: outer loop missing - internal loop is being executed only once per c.

--- ida/eval.py
+++ ida/eval.py
@@ -229,7 +229,7 @@ class NCV:
 
     def calc_hyperparams( self
                         , dataset
-                        , c_grid=np.logspace( start=0, stop=2, num=50 ) ):
+                        , c_grid=np.logspace( start=0, stop=2, num=50 ) - 1 ):  # [0-99]
         '''Perform a grid search in hyperparameter space.'''
         risk = []
         for c in c_grid:
