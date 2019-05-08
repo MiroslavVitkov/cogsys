f3d616ddc2420c0b9fcfbd5e66542c8bf5c2b631
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-08 20:24:36 +0200

    [pics] Refactored, but still we are writing to disk and reading back the image. Barbaric!

--- pics/space/space.py
+++ pics/space/space.py
@@ -9,16 +9,13 @@ from io import StringIO
 from visual_genome import api as vg
 
 
-fig = plt.gcf()
-fig.set_size_inches(18.5, 10.5)
-
 def visualize_regions(image, regions):
     response = requests.get(image.url)
-    f = open('kur.jpg', 'bw')
+    f = open('tmp.jpg', 'bw')
     f.write(response.content)
-    file = StringIO(str(response.content))
-    img = Image.open('kur.jpg')
+    img = Image.open('tmp.jpg')
     plt.imshow(img)
+
     ax = plt.gca()
     for region in regions:
         ax.add_patch(Rectangle((region.x, region.y),
@@ -33,6 +30,7 @@ def visualize_regions(image, regions):
     plt.show()
 
 
+fig = plt.gcf()
 image = vg.get_image_data(id=61512)
 regions = vg.get_region_descriptions_of_image(id=61512)
 visualize_regions(image, regions[:8])
