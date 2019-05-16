85baef4205df4aa2ec51662964e9eee17df6fcb6
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-16 09:06:12 +0200

    [pics] Caching system for accessing the dataset offline is finished.

--- pics/space/space.py
+++ pics/space/space.py
@@ -8,9 +8,12 @@ from os.path import isfile
 from PIL import Image
 import requests
 import urllib
-from visual_genome import api as vg
+from visual_genome import api as vgr
 from visual_genome import local as vgl
 from zipfile import ZipFile
+
+
+DATA_DIR = './data/'
 
 
 def visualize_regions(image, regions):
@@ -37,13 +40,30 @@ def visualize_regions(image, regions):
     plt.show()
 
 
-def get_next(ids=None):
+def get_next_remote(ids=None):
     if ids is None:
-        ids = vg.get_all_image_ids()
+        ids = vgr.get_all_image_ids()
+
     for id in ids:
-        image = vg.get_image_data(id)
-        regions = vg.get_region_descriptions_of_image(id)
-        graph = vg.get_scene_graph_of_image(id)
+        image = vgr.get_image_data(id)
+        regions = vgr.get_region_descriptions_of_image(id)
+        graph = vgr.get_scene_graph(id)
+        yield image, regions, graph
+
+
+def get_next_local(ids=None):
+    images = vgl.get_all_image_data(DATA_DIR)
+    all_regions = vgl.get_all_region_descriptions(DATA_DIR)  # slow
+    if ids is None:
+        ids = [i for i in range(1, len(images))]
+
+    for id in ids:
+        image = images[id-1]
+        regions = all_regions[id-1]
+        graph = vgl.get_scene_graph(id
+                                   ,images=DATA_DIR
+                                   ,image_data_dir=DATA_DIR+'/by-id/'
+                                   ,synset_file=DATA_DIR+'/synsets.json')
         yield image, regions, graph
 
 
@@ -54,7 +74,7 @@ def download_zip(path, url):
         zip.extractall(path)
 
 
-def download_dataset(path='data/'):
+def download_dataset(path=DATA_DIR):
     if not isfile(path+'/image_data.json'):
         download_zip(path, 'http://visualgenome.org/static/data/dataset/image_data.json.zip')
     if not isfile(path+'/region_descriptions.json'):
@@ -66,19 +86,8 @@ def download_dataset(path='data/'):
         download_zip(path, 'http://visualgenome.org/static/data/dataset/synsets.json.zip')
 
 
-download_dataset()
-
-#scene_graphs = vgl.get_scene_graphs(start_index=0, end_index=-1, min_rels=1,
-#                                    data_dir='data/', image_data_dir='data/by-id/')
-
-#_, _, graph = get_next(ids=[1])
-#r = graph.relationships[0]
-#print(r, 'OBJ', r.object, 'PRED', r.predicate, 'SUBJ', r.subject, 'SYNSET', r.synset)
-
-
-#for image, regions in get_next([1]):
-#    print('Processing image with id', image.id)
-#    for region in regions:
-#        print(region.phrase)
-#    #visualize_regions(image, regions[:8])
-
+for image, regions, graph in get_next_local(ids=[1]):
+    print('Processing image with id', image.id)
+    r = graph.relationships[0]
+    print(r, 'OBJ', r.object, 'PRED', r.predicate, 'SUBJ', r.subject, 'SYNSET', r.synset)
+    visualize_regions(image, regions[:8])
