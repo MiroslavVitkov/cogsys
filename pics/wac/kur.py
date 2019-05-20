c551978c382bf2bd2366f009fcb43e65fb474c32
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-20 19:51:32 +0200

    [pics] Problem formulation for this week's homework.

--- pics/wac/kur.py
+++ pics/wac/kur.py
@@ -1,42 +1,24 @@
 #!/usr/bin/env python3
 
 
+'''
+This module investigates colour detection over Visual Genome image regions.
+
+Firstly, attributes are extracted from region graphs.
+Then a naive word search is performed to detect colours.
+The associated images are fetched and relevant regions - cropped.
+Cropped regions are randomly split into a training and test sets.
+A multi-label classifier is trained and evaluated.
+Prevision and accuracy are reported,
+along with some of the true positives and false positives.
+'''
+
+
 from PIL import Image
 
 
-class Region:
-    '''Recatangular region, top left is (0, 0).'''
-    def __init__(me, x, y, w, h):
-        assert x >=0 and y >=0 and w > 0 and h > 0
-        me.x = x
-        me.y = y
-        me.w = w
-        me.h = h
-
-
-    @staticmethod
-    def from_image(image):
-        w, h = image.size()
-        return Region(0, 0, w, h)
-
-
-    def contains(me, region):
-#        if me.x <= region.x and
-#           me.y <= region.y and
-#           me.x + me.w >= region.x + region.w and
-#           me.y + me.h >= region.y + region.h:
-#            return True
-#        else:
-#            return False
-        pass
-
-
-def crop(image, region):
-    assert Region.from_image(image).contains(region)
-    r = region
-    box = (r.x, r.y, r.x+r.w, r.y+r.h)
-    img = image.crop(box)
-    return img
+# Crop and rescale images into this size.
+PATCH_SIZE = (224, 224)
 
 
 def main():
