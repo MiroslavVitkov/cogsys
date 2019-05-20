e1c4a88f64e4de87c68f38c0a080ad13846cae7e
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-20 19:15:27 +0200

    [pics] Deleted imports code and wrote resizing code, which is also to be deleted, because PIL.Image.resize() does it all.

--- pics/wac/kur.py
+++ pics/wac/kur.py
@@ -1,38 +1,51 @@
 #!/usr/bin/env python3
 
 
-def read_config(path):
-    import configparser as c
-    config = c.ConfigParser()
-    with open(path, 'r', encoding='utf-8') as f:
-        config.read_file(f)
-    return config
+from PIL import Image
 
 
-def load_wac(config, sempix_path):
-    h = config.get('DSGV-PATHS', 'dsgv_home')
-    import sys
-    sys.path.append(h + '/Utils')
-    import utils
-    sys.path.append(h + '/WACs/WAC_Utils')
-    import wac_utils
-    sys.path.append('/Users/das/work/svn/Gits/p_Public/sempix/Common')
-    import data_utils
+class Region:
+    '''Recatangular region, top left is (0, 0).'''
+    def __init__(me, x, y, w, h):
+        assert x >=0 and y >=0 and w > 0 and h > 0
+        me.x = x
+        me.y = y
+        me.w = w
+        me.h = h
 
-    # I have zero idea why are we doing the following.
-    # It's just the way the original Jupiter Notebook is written.
-    from importlib import reload
-    reload(wac_utils)
 
-    return utils, wac_utils, data_utils
+    @staticmethod
+    def from_image(image):
+        w, h = image.size()
+        return Region(0, 0, w, h)
+
+
+    def contains(me, region):
+#        if me.x <= region.x and
+#           me.y <= region.y and
+#           me.x + me.w >= region.x + region.w and
+#           me.y + me.h >= region.y + region.h:
+#            return True
+#        else:
+#            return False
+        pass
+
+
+def crop(image, region):
+    assert Region.from_image(image).contains(region)
+    r = region
+    box = (r.x, r.y, r.x+r.w, r.y+r.h)
+    img = image.crop(box)
+    return img
 
 
 def main():
-    config = read_config('/home/vorac/proj/wac/clp-vision/Config/default.cfg')
-    utils, wac_utils, data_utils = load_wac(config, '/home/vorac/proj/wac/sempix/Common')
+    print('putka')
+    img = Image.open('/home/vorac/2019-05-08-190834_1920x1080_scrot.png')
+    print('kur')
+    img = resize(img, 224, 224)
+    img.show()
 
-
-
-if __name__ == '__main __':
+if __name__ == '__main__':
     main()
 
