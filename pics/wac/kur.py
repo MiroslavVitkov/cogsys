fab54df6630b3fd6ccea99f84dcf54748aa581d6
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-20 10:07:56 +0200

    [pics] Importing everything now.

--- pics/wac/kur.py
+++ pics/wac/kur.py
@@ -9,8 +9,27 @@ def read_config(path):
     return config
 
 
+def load_wac(config, sempix_path):
+    h = config.get('DSGV-PATHS', 'dsgv_home')
+    import sys
+    sys.path.append(h + '/Utils')
+    import utils
+    sys.path.append(h + '/WACs/WAC_Utils')
+    import wac_utils
+    sys.path.append('/Users/das/work/svn/Gits/p_Public/sempix/Common')
+    import data_utils
+
+    # I have zero idea why are we doing the following.
+    # It's just the way the original Jupiter Notebook is written.
+    from importlib import reload
+    reload(wac_utils)
+
+    return utils, wac_utils, data_utils
+
+
 def main():
-    read_config('/home/vorac/proj/wac/clp-vision/Config/default.cfg')
+    config = read_config('/home/vorac/proj/wac/clp-vision/Config/default.cfg')
+    utils, wac_utils, data_utils = load_wac(config, '/home/vorac/proj/wac/sempix/Common')
 
 
 
